9dc8265f033422655d4042a6c88f5206d2546553
Ana Matei <anamatei.099@gmail.com>
2022-03-18 13:56:30 +0000

    Changing Footer to Bootstrap

--- netflix/src/components/Footer.jsx
+++ netflix/src/components/Footer.jsx
@@ -1,50 +1,62 @@
 import React from "react";
-import { Box, Container, Row, Column, FooterLink, Icon } from "./FooterStyles";
 
-import { Button} from "react-bootstrap";
+
+import { Button,Container, ListGroup, ListGroupItem, Row, Col} from "react-bootstrap";
 
 const MyFooter = () => {
+
+
+
+    
   return (
-    <Box>
-     <Container>
-       <Icon>
+  <Container fluid style={{backgroundColor:"black"}}>
+     <Container className="pt-3 pb-3">
+       
             <i class="bi bi-facebook mx-1"></i>
             <i class="bi bi-instagram mx-1"></i>
         <i class="bi bi-twitter mx-1"></i>
         <i class="bi bi-youtube mx-1"></i>
-       </Icon>
+       
        </Container>
      
-      <Container>
+      <Container style={{backgroundColor:"black"}}>
         <Row>
-          <Column>
-            <FooterLink>Audio and Subtitles</FooterLink>
-            <FooterLink >Media Center</FooterLink>
-            <FooterLink >Privacy</FooterLink>
-            <FooterLink >Contact us</FooterLink>
-            <Button variant="outline-light">Service Code</Button>
-          </Column>
-          <Column>
-            <FooterLink>Audio Description</FooterLink>
-            <FooterLink>Investor Relations</FooterLink>
-            <FooterLink >Legal Notices</FooterLink>
-          </Column>
-          <Column>
-            <FooterLink>Help Center</FooterLink>
-            <FooterLink >Jobs</FooterLink>
-            <FooterLink >Cookie</FooterLink>
-            <FooterLink >Preferences</FooterLink>
-          </Column>
-          <Column>
-            <FooterLink >Gift Cards</FooterLink>
-            <FooterLink >Terms of Use</FooterLink>
-            <FooterLink>Corporate</FooterLink>
-            <FooterLink>Information</FooterLink>
-          </Column>
+         <Col>
+              <ListGroup variant="flush">
+                <ListGroupItem style={{backgroundColor:"black"}} className="text-white">Audio and Subtitles</ListGroupItem>
+                <ListGroupItem style={{backgroundColor:"black"}} className="text-white">Media Center</ListGroupItem>
+                <ListGroupItem style={{backgroundColor:"black"}} className="text-white">Privacy</ListGroupItem>
+                <ListGroupItem style={{backgroundColor:"black"}} className="text-white">Contact us</ListGroupItem>
+                <Button variant="outline-light">Service Code</Button>
+              </ListGroup>
+         </Col>
+        <Col>
+              <ListGroup variant="flash">
+                <ListGroupItem >Audio Description</ListGroupItem>
+                <ListGroupItem>Investor Relations</ListGroupItem>
+                <ListGroupItem >Legal Notices</ListGroupItem>
+              </ListGroup>
+        </Col>
+          <Col>
+              <ListGroup variant="flash">
+                <ListGroupItem>Help Center</ListGroupItem>
+                <ListGroupItem >Jobs</ListGroupItem>
+                <ListGroupItem >Cookie</ListGroupItem>
+                <ListGroupItem>Preferences</ListGroupItem>
+              </ListGroup>
+          </Col>
+          <Col>
+              <ListGroup variant="flash">
+                <ListGroupItem >Gift Cards</ListGroupItem>
+                <ListGroupItem >Terms of Use</ListGroupItem>
+                <ListGroupItem>Corporate</ListGroupItem>
+                <ListGroupItem>Information</ListGroupItem>
+              </ListGroup>
+          </Col>
         </Row>
       </Container>
-    </Box>
-
+    
+      </Container>
 
   );
 };
