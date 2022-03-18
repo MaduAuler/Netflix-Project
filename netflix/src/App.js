c09fa0eb6df324517546f25ee698fc21011dab22
MaduBitencourt <bitencourtmadu@gmail.com>
2022-03-18 13:37:31 +0100

    resolving conflits

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
+import Gallery from './components/Gallery'
+import Footer from './components/Footer'
 import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      
       <NavBar />
+      <Gallery/>
+      <Footer/>
     </div>
   );
 }
