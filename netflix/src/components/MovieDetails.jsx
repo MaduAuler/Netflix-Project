5eacde4f10201c00d66581b41c339b7fb9f0635d
Ana Matei <anamatei.099@gmail.com>
2022-03-23 23:20:49 +0000

    Update MovieDetails

--- netflix/src/components/MovieDetails.jsx
+++ netflix/src/components/MovieDetails.jsx
@@ -1,50 +1,89 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { Container, Card,} from "react-bootstrap";
+import { Container, Card,Row,Col} from "react-bootstrap";
 
 const MovieDetails = () => {
-  /*const params = useParams();
+  const params = useParams();
   console.log("PARAMS", params);
 
    const [movieToShow, setMovieToShow] = useState(null);
+   const [commentsToShow, setCommentsToShow] = useState([])
 
   useEffect(() => {
+
    getMovieDetails()
+   getComments()
   }, [])
  
-   const getMovieDetails = async () => {
+  const getMovieDetails = async () => {
     try {
-      let response = await fetch(
-        "https://www.omdbapi.com/?apikey=24ad60e9&i=" + params.movieId
-      );
-      if (response.ok) {
-        let data = await response.json();
-        setMovieToShow(data);
-      } else {
-        alert("We got an error");
+      let response = await fetch("http://www.omdbapi.com/?apikey=9448849c&i=" + params.movieID )
+        if(response.ok) {
+            const data = await response.json()
+           console.log(setMovieToShow(data))
+              
+          } else {
+              console.log('error while fetching')
+          }
+        } catch(e) {
+          console.log(e)
+        }
       }
-    } catch (error) {
-      console.log(error);
-    }
-  };
- */
+ 
+
+      const getComments = async () => {
+        
+            try {
+                let response = await fetch(
+                  "https://striveschool-api.herokuapp.com/api/comments/" + params.movieID,
+                  {
+                    headers: {
+                      Authorization:
+                        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MThkMzg2YTVmMzRhZDAwMTUzOWYxOWEiLCJpYXQiOjE2NDgwNDI2OTIsImV4cCI6MTY0OTI1MjI5Mn0.GPyJyTwmqj4B9yTk7otvaOvjstZ9iEaOdjvdWfl-sh0",
+                    },
+                  }
+                );
+                  if(response.ok) {
+                      const data = await response.json()
+                    console.log(setCommentsToShow(data))
+                        
+                    } else {
+                        console.log('error while fetching')
+                    }
+                  } catch(e) {
+                    console.log(e)
+                  }
+          
+      }
 
 
   return (
-  
     <Container>
-      <h1>MOVIE DETAILS</h1>
-      <Card >
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </Container>
+    <h3>Movie Details</h3>
+    {movieToShow && (
+      <>
+        <Row className="justify-content-center my-2">
+          <Col md={6}>
+            <Card>
+              <Card.Img variant="top" src={movieToShow.Poster} />
+              <Card.Body className="text-center">
+                <Card.Title>{movieToShow.Title}</Card.Title>
+                   <Card.Text>
+                    {movieToShow.Plot}
+                    {movieToShow.Actors}
+                  </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row className="justify-content-center my-2">
+          <Col md={6}>
+           {/*  <DishComments dish={pastaToShow} /> */}
+          </Col>
+        </Row>
+      </>
+    )}
+  </Container>
     
   );
 };
